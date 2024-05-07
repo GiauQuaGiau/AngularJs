e1b107a34743350dedfbd3d53f6e41cee0605568
NKDuy2706 <nkduydhtv@gmail.com>
2024-05-07 14:34:13 +0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,369 @@
+
+products = [
+    {
+        id: 1,
+        tenSanPham: "Tai nghe Bluetooth Gaming",
+        gia: 500000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "taingheblue.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Tai nghe không dây kết nối Bluetooth",
+        chiTietSanPham: "Tai nghe Bluetooth với chất lượng âm thanh tuyệt vời và thời gian pin lâu dài. Có thể sử dụng trong khoảng cách xa và điều khiển bằng cử chỉ. Đệm tai nghe êm ái và có thể tháo rời để vệ sinh.",
+    },
+    {
+        id: 2,
+        tenSanPham: "Ấm đun nước siêu tốc",
+        gia: 350000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "amDunNuoc.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Ấm đun nước siêu tốc, tiết kiệm thời gian.",
+        chiTietSanPham: "Ấm đun siêu tốc với công suất mạnh mẽ, giúp đun nước nhanh chóng. Thiết kế hiện đại với các tính năng an toàn như tự động tắt khi nước sôi và khóa an toàn trẻ em. Chất liệu thép không gỉ đảm bảo độ bền và vệ sinh.",
+    },
+    {
+        id: 3,
+        tenSanPham: "Giá đỡ laptop 14, 15inch",
+        gia: 200000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "giadoLap.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Giá đỡ laptop cầm tay, dễ dàng di chuyển.",
+        chiTietSanPham: "Giá đỡ laptop linh hoạt, giúp bạn làm việc hiệu quả với máy tính xách tay. Chất liệu nhôm bền đẹp và có thể điều chỉnh độ cao phù hợp. Thanh chắn đảm bảo máy tính không bị rơi hay trượt.",
+    },
+    {
+        id: 4,
+        tenSanPham: "Kem dưỡng da, dưỡng ẩm",
+        gia: 150000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "kem.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Kem dưỡng da chính hãng.",
+        chiTietSanPham: "Kem dưỡng da giúp giữ ẩm da một cách tối ưu.giúp dưỡng ẩm và làm dịu tình trạng da kích ứng, tổn thương, hỗ trợ phục hồi làn da. Được khuyên dùng sau các liệu trình điều trị thẩm mỹ & kích ứng da nhẹ ở người lớn, trẻ em và trẻ sơ sinh",
+    },
+    {
+        id: 5,
+        tenSanPham: "Cửu âm chân kinh",
+        gia: 120000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "sach.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Sách về cuộc sống và học tập.",
+        chiTietSanPham: "Những cuốn sách bổ ích với nhiều kiến thức hữu ích cho cuộc sống hàng ngày. Được viết bởi các tác giả uy tín và nổi tiếng. Phù hợp cho mọi lứa tuổi và sở thích đọc sách.",
+    },
+    {
+        id: 6,
+        tenSanPham: "Sữa tắm lifebuoy",
+        gia: 80000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "suatam.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Sữa tắm dưỡng ẩm cho da.",
+        chiTietSanPham: "Sữa tắm giúp da mềm mịn và thơm phức suốt cả ngày. Công thức dịu nhẹ, không gây kích ứng da và dễ dàng rửa sạch. Chăm sóc và nuôi dưỡng da mỗi lần tắm.",
+    },
+    {
+        id: 7,
+        tenSanPham: "Thẻ nhớ Sandisk 128gb",
+        gia: 250000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "theNho.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Thẻ nhớ dung lượng lớn cho điện thoại và máy tính.",
+        chiTietSanPham: "Thẻ nhớ với dung lượng cao giúp bạn lưu trữ nhiều dữ liệu quan trọng. Tốc độ đọc/ghi nhanh, hỗ trợ lưu trữ ảnh, video, tệp tin, và ứng dụng. Bảo vệ dữ liệu an toàn và bền vững.",
+    },
+    {
+        id: 8,
+        tenSanPham: "Kem vệ sinh giày da, túi da",
+        gia: 60000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "kemVsGiay.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Kem vệ sinh giày chuyên nghiệp.",
+        chiTietSanPham: "Kem vệ sinh giày giúp giữ cho đôi giày luôn sạch sẽ và bền đẹp. Công thức đặc biệt loại bỏ các vết bẩn và mùi khó chịu. Thích hợp cho mọi loại giày dép, da, và vải.",
+    },
+    {
+        id: 9,
+        tenSanPham: "Laptop LG Gram 2022 (8/256)",
+        gia: 24500000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "laptop.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Laptop chất lượng cao với hiệu suất mạnh mẽ. (Intel Core i3-1220P/8GB/256GB/VGA Intel UHD Graphics/14 inch WQXGA/Màu Đen/Non- OS)",
+        chiTietSanPham: "Laptop mỏng nhẹ, thiết kế sang trọng và cấu hình cao. Màn hình Full HD, bộ vi xử lý nhanh, RAM lớn và ổ cứng SSD giúp xử lý công việc mượt mà và lưu trữ dữ liệu nhanh chóng.",
+    },
+    {
+        id: 10,
+        tenSanPham: "Apple iPad Air (5th Gen) Wi-Fi, 2022",
+        gia: 12260000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "ipad.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Tablet phù hợp cho công việc và giải trí. Chip Apple M1 nổi bật với hiệu năng vận hành mạnh mẽ và các tính năng máy học cao cấp, hỗ trợ tối đa tác vụ biên tập video 4K, soạn bài thuyết trình đẹp mắt hay thiết kế mô hình 3D - mọi tác vụ đều thật dễ dàng.",
+        chiTietSanPham: "Tablet với màn hình Retina sắc nét, bút stylus tiện lợi và hiệu năng mạnh mẽ. Chạy hệ điều hành iOS hoặc Android cho nhiều ứng dụng và tùy chọn tuỳ chỉnh. Thiết bị giờ đã hỗ trợ Magic Keyboard và Apple Pencil (thế hệ thứ 2) (2), Touch ID nhanh nhạy, dễ sử dụng, bảo mật cao, camera sau 12MP góc siêu rộng hỗ trợ Center Stage và camera FaceTime HD 12MP góc rộng, cổng kết nối USB-C hỗ trợ sạc và phụ kiện, thời lượng pin bền bỉ cả ngày (3), công nghệ Wi-Fi 6. Với iPad Air 5 trong tay, bạn đã có đủ sức mạnh để hiện thực hóa mọi ý tưởng của bản thân.",
+    },
+    {
+        id: 11,
+        tenSanPham: "Apple iPhone 14 Pro Max 256GB",
+        gia: 26990000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "smartphone.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "iPhone 14 Pro Max. Bắt trọn chi tiết ấn tượng với Camera Chính 48MP. Trải nghiệm iPhone theo cách hoàn toàn mới với Dynamic Island và màn hình Luôn Bật. Công nghệ an toàn quan trọng – Phát Hiện Va Chạm1 thay bạn gọi trợ giúp khi cần kíp",
+        chiTietSanPham: "Smartphone với màn hình AMOLED sắc nét, camera đa chức năng chụp ảnh và quay video chất lượng cao. Hỗ trợ 4G/5G, đọc vân tay, và pin lâu trụi.",
+    },
+    {
+        id: 12,
+        tenSanPham: "Quần áo thể thao Adidas",
+        gia: 690000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "quanao.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Quần áo thời trang và thoải mái.",
+        chiTietSanPham: "Quần áo chất liệu cotton mềm mại, thoáng mát và dễ phối đồ. Thiết kế trẻ trung, phong cách và thời thượng. Phù hợp cho mọi dịp và hoàn cảnh.",
+    },
+    {
+        id: 13,
+        tenSanPham: "Samsung Galaxy Z Fold 5",
+        gia: 39990000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "zfold5.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Thiết kế nhỏ gọn cùng bản lề Flex mới cho khả năng gập không kẽ hở, Chipset Snapdragon 8 Gen 2 for Galaxy",
+        chiTietSanPham: "Samsung Z Fold5 được trang bị con chip Snapdragon 8 Gen 2 mạnh mẽ, bộ nhớ RAM 12GB, ống kính chính 50MP, camera selfie bên ngoài 10MP, camera màn hình chính 4MP. Bên cạnh đó, điện thoại Samsung này còn sở hữu tấm nền Dynamic AMOLED 2X, kích thước màn hình 7.6 inch khi mở, độ phân giải 2K cho hình ảnh rõ nét cùng với dung lượng pin 4.400 mAh thoải mái sử dụng.",
+    },
+    {
+        id: 14,
+        tenSanPham: "Điện thoại Xiaomi Redmi 9A",
+        gia: 2390000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "redmi9a.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Thiết kế bóng bẩy, thời trang, Pin siêu trâu, thoải mái trải nghiệm, Vi xử mới tối ưu hiệu năng, tiết kiệm pin.",
+        chiTietSanPham: "Xiaomi Redmi 9A sở hữu một màn hình tràn viền kích thước cực lớn 6.53 inch với thiết kế theo kiểu giọt nước quen thuộc, đi theo xu hướng chung của những mẫu smartphone hiện nay. Màn hình công nghệ IPS LCD độ phân giải HD+ đủ để bạn nhìn thấy các thông tin, hình ảnh rõ ràng với màu sắc tươi tắn.",
+    },
+    {
+        id: 15,
+        tenSanPham: "Apple iPad Pro 11-inch M2 Wi-Fi, 2022",
+        gia: 21190000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "ipadPro.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Mô tả sản phẩm Apple iPad Pro 11-inch M2 Wi-Fi, 2022",
+        chiTietSanPham: "Chi tiết sản phẩm Apple iPad Pro 11-inch M2 Wi-Fi, 2022",
+      },
+      {
+        id: 16,
+        tenSanPham: "Samsung Galaxy A54",
+        gia: 7990000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "a54.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Mô tả sản phẩm Samsung Galaxy A54",
+        chiTietSanPham: "Chi tiết sản phẩm Samsung Galaxy A54",
+      },
+      {
+        id: 17,
+        tenSanPham: "Apple iPad 10.2-inch (9th Gen)",
+        gia: 6990000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "ipadGen9.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Mô tả sản phẩm Apple iPad 10.2-inch (9th Gen) Wi-Fi, 2021",
+        chiTietSanPham: "Chi tiết sản phẩm Apple iPad 10.2-inch (9th Gen) Wi-Fi, 2021",
+      },
+      {
+        id: 18,
+        tenSanPham: "Samsung Galaxy S23 Ultra 5G",
+        gia: 24590000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "s23ultra.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Mô tả sản phẩm Samsung Galaxy S23 Ultra 5G (8GB/256GB)",
+        chiTietSanPham: "Chi tiết sản phẩm Samsung Galaxy S23 Ultra 5G (8GB/256GB)",
+      },
+      {
+        id: 19,
+        tenSanPham: "MacBook Air M2 2022",
+        gia: 28960000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "macAir2022.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Mô tả sản phẩm MacBook Air M2 2022",
+        chiTietSanPham: "Chi tiết sản phẩm MacBook Air M2 2022",
+      },
+      {
+        id: 20,
+        tenSanPham: "Lenovo IdeaPad Gaming 3 R5-5600H | 8GB | 512GB",
+        gia: 25690000,
+        luotBan: Math.floor(Math.random() * (120 - 20 + 1)) + 80,
+        urlAnh: "lenovoIdeapad.webp",
+        rating: (Math.random() * (5.0 - 3.0) + 3.0).toFixed(1),
+        moTaSanPham: "Mô tả sản phẩm Lenovo IdeaPad Gaming 3 R5-5600H | 8GB | 512GB | RTX 3050",
+        chiTietSanPham: "Chi tiết sản phẩm Lenovo IdeaPad Gaming 3 R5-5600H | 8GB | 512GB | RTX 3050",
+      },
+    // Thêm các sản phẩm khác vào đây
+];
+
+danhmuc = [
+    {
+        id: 1,
+        name: "Đồ Chơi - Mẹ & Bé",
+        img: "toy.png",
+        slug: "toy"
+    },
+    {
+        id: 2,
+        name: "Thiết bị điện tử",
+        img: "dientu.png",
+        slug: "dientu"
+    },
+    {
+        id: 3,
+        name: "Điện Thoại",
+        img: "smartphone.png",
+        slug: "smartphone"
+    },
+    {
+        id: 4,
+        name: "Máy Tính Bảng",
+        img: "tablet.png",
+        slug: "tablet"
+    },
+    {
+        id: 5,
+        name: "Máy tính xách tay",
+        img: "laptop.png",
+        slug: "laptop"
+    },
+    {
+        id: 6,
+        name: "Thời trang nam",
+        img: "thoitrangnam.png",
+        slug: "thoitrang"
+    },
+    {
+        id: 7,
+        name: "Thời trang nữ",
+        img: "thoitrang.png",
+        slug: "thoitrang"
+    },
+    {
+        id: 8,
+        name: "Nhà sách Tiki",
+        img: "sach.png",
+        slug: "sach"
+    },
+    {
+        id: 9,
+        name: "Làm đẹp - Sức khỏe",
+        img: "lamdep.png",
+        slug: "sk-ld"
+    },
+    {
+        id: 10,
+        name: "Giày - Dép",
+        img: "daydepnua.png",
+        slug: "giaydep"
+    },
+    {
+        id: 11,
+        name: "Điện gia dụng",
+        img: "giadung.png",
+        slug: "giadung"
+    },
+    {
+        id: 12,
+        name: "Du lịch",
+        img: "vali.png",
+        slug: "dulich"
+    },
+    {
+        id: 13,
+        name: "Phụ kiện thời trang",
+        img: "kinh.png",
+        slug: "kinh"
+    },
+    {
+        id: 14,
+        name: "Đồng hồ và Trang sức",
+        img: "dongho.png",
+        slug: "dongho"
+    },
+    {
+        id: 15,
+        name: "Bách Hóa Online",
+        img: "bachhoa.png",
+        slug: "bachhoa"
+    },
+    {
+        id: 16,
+        name: "NGON",
+        img: "ngon.png",
+        slug: "ngon"
+    },
+]
+
+noibat = [
+    {
+        id: 1,
+        name: "Tiki ChatGPT",
+        img: "chatgpt.png",
+        slug: "chatgpt"
+    },
+    {
+        id: 2,
+        name: "Astra Reward",
+        img: "astra.png",
+        slug: "astra"
+    },
+    {
+        id: 3,
+        name: "Tiki Exchange",
+        img: "aschange.png",
+        slug: "aschange"
+    },
+    {
+        id: 4,
+        name: "Giá Rẻ Mỗi Ngày",
+        img: "saleday.png",
+        slug: "saleday"
+    },
+    {
+        id: 5,
+        name: "Xả kho",
+        img: "xakho.png",
+        slug: "xakho"
+    },
+    {
+        id: 6,
+        name: "Mã giảm giá",
+        img: "mgg.png",
+        slug: "mgg"
+    },
+    {
+        id: 7,
+        name: "Ưu đãi thẻ, ví",
+        img: "vi.png",
+        slug: "vi"
+    },
+    {
+        id: 8,
+        name: "Đóng tiền, nạp thẻ",
+        img: "napthe.png",
+        slug: "napthe"
+    },
+    {
+        id: 9,
+        name: "Mua trước trả sau",
+        img: "trasau.png",
+        slug: "trasau"
+    },
+    {
+        id: 10,
+        name: "Bảo hiểm Tiki360",
+        img: "baohiem.png",
+        slug: "baohiem"
+    },
+
+]
+
+
